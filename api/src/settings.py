71bbce9b4ff88f49b4e1418bbaeebf856caa1f65
jmbenck <jmbenck@gmail.com>
2019-11-23 22:15:42 -0300

    Funcionando UPDATE

--- api/src/settings.py
+++ api/src/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django_filters',
     'usuarios',
     'suit',
+    'corsheaders',
 ]
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
@@ -53,7 +54,22 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
+
+CORS_ALLOW_METHODS = [
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+]
+
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = True
+
 
 ROOT_URLCONF = 'src.urls'
 
