3247a6275eb87be46dde75f3828b23330c395148
jmbenck <jmbenck@gmail.com>
2020-04-29 01:00:25 -0300

    Mudando droplet DigitalOcean

--- api/src/router.py
+++ api/src/router.py
@@ -2,6 +2,6 @@ from usuarios.api.viewsets import UsuarioView, EscolaView, CidadeView
 from rest_framework import routers
 
 router = routers.DefaultRouter()
-router.register('usuario', UsuarioView, base_name='usuario')
+router.register('usuario', UsuarioView)
 router.register('escola', EscolaView)
 router.register('cidade', CidadeView)
