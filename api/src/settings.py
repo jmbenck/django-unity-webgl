497db93dd8c1eb088a1a51acf5806ab786be45cf
jmbenck <jmbenck@gmail.com>
2020-05-05 19:01:15 -0300

    Update search ranking

--- api/src/settings.py
+++ api/src/settings.py
@@ -128,7 +128,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'mp',
         'USER': 'root',
-        'PASSWORD': 'root',
+        'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '3306',
     }
